c026870059563bfaf36522bf935800f95a0b39f4
Vlad Krupen <air7360@gmail.com>
2023-11-04 16:59:20 +0300

    navigationbar removed for the SecondViewController

--- Homework_Navigation/SecondViewController.swift
+++ Homework_Navigation/SecondViewController.swift
@@ -11,6 +11,9 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+
 
         // Do any additional setup after loading the view.
     }
